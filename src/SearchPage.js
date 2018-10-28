b27ada822f0671f062fa99bf5e7b99ed9b24da7e
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2018-10-27 21:59:04 -0500

    Adding styles work complete

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -9,16 +9,45 @@ import { StyleSheet, Text,
 const styles = StyleSheet.create({
   description: {
     fontSize: 18,
+    marginBottom: 20,
     textAlign: 'center',
     color: '#656565',
-    marginTop: 65,
   },
   container:{
     padding: 30,
     marginTop: 65,
     alignItems: 'center',
   },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  }
 });
+
+const layout = {
+  searchInput: {
+    underlineColorAndroid: 'transparent',
+    style: styles.searchInput,
+    placeholder: 'Buscar por nombre o C.P.',
+  },
+  searchButton: {
+    onPress: () => {},
+    color: '#48BBEC',
+    title: 'Buscar',
+  },
+};
 
 type Props = {};
 
@@ -36,6 +65,10 @@ export default class SearchPage extends Component<Props> {
           <Text style={styles.description}>
             Búsqueda por ubicación o código postal.
           </Text>
+          <View style={styles.flowRight}>
+            <TextInput {...layout.searchInput}/>
+            <Button {...layout.searchButton}/>
+          </View>
         </View>
       );
     }
